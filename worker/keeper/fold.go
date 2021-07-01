e6da4a72f815d907fa72ccefa2a99e246927358b
yiplee <guoyinl@gmail.com>
2021-07-01 15:45:50 +0800

    [keeper] adjust fold duration to 90s

--- worker/keeper/fold.go
+++ worker/keeper/fold.go
@@ -6,22 +6,36 @@ import (
 
 	"github.com/fox-one/pando/core"
 	"github.com/fox-one/pando/pkg/uuid"
+	"github.com/fox-one/pkg/logger"
 )
 
 func (w *Keeper) foldCats(ctx context.Context) error {
-	dur := time.Millisecond
-
 	for {
 		select {
 		case <-ctx.Done():
 			return ctx.Err()
-		case t := <-time.After(dur):
-			trace := uuid.MD5(t.Truncate(time.Minute).Format(time.RFC3339Nano))
+		case t := <-time.After(time.Second):
+			cats, err := w.cats.List(ctx)
+			if err != nil {
+				logger.FromContext(ctx).WithError(err).Errorln("cats.List")
+				break
+			}
 
-			if err := w.handleTransfer(ctx, trace, core.ActionCatFold, uuid.Zero); err != nil {
-				dur = time.Second
-			} else {
-				dur = time.Minute
+			if len(cats) == 0 {
+				break
+			}
+
+			fold := false
+			for _, cat := range cats {
+				if t.Sub(cat.Rho) > time.Second*90 {
+					fold = true
+					break
+				}
+			}
+
+			if fold {
+				trace := uuid.MD5(t.Truncate(time.Minute).Format(time.RFC3339Nano))
+				_ = w.handleTransfer(ctx, trace, core.ActionCatFold, uuid.Zero)
 			}
 		}
 	}
